c30fed82b48d2d8623184c4ff736c9483d153000
Floweryu <im.zhangjunfeng@qq.com>
2021-12-05 14:03:49 +0800

    异步发送消息

---
+++ rocketmq/src/main/java/com/floweryu/rocketmq/async/AsyncConsumer.java
@@ -0,0 +1,41 @@
+package com.floweryu.rocketmq.async;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @author Floweryu
+ * @date 2021/12/5 11:08
+ */
+public class AsyncConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("async_consumer_group");
+
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("106.15.42.148:9876");
+
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+        consumer.subscribe("TopicTest", "*");
+
+        // 注册回调实现类来处理从broker拉取回来的消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), list);
+                // 标记该消息已经被成功消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
