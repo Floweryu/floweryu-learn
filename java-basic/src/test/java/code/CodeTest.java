087eff3836020619bdf9926016ed918628283e6f
zhangjunfeng <zhangjunfeng1@xiaomi.com>
2022-03-29 16:59:19 +0800

    每日一题

--- java-basic/src/test/java/code/CodeTest.java
+++ java-basic/src/test/java/code/CodeTest.java
@@ -219,9 +219,32 @@ public class CodeTest {
         }
         return true;
     }
+
+    /**
+     * T T F T T F T T
+     * @param answerKey
+     * @param k
+     * @return
+     */
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        return Math.max(maxConsecutiveChar(answerKey, k, 'T'), maxConsecutiveChar(answerKey, k, 'F'));
+    }
+
+    public int maxConsecutiveChar(String answerKey, int k, char ch) {
+        int n = answerKey.length();
+        int ans = 0;
+        for (int left = 0, right = 0, sum = 0; right < n; right++) {
+            sum += answerKey.charAt(right) != ch ? 1 : 0;
+            while (sum > k) {
+                sum -= answerKey.charAt(left++) != ch ? 1 : 0;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
     
     @Test
     public void codeTest() {
-        System.out.println(hasAlternatingBits(5));
+        System.out.println(0);
     }
 }
