7abc9c667ddf37c7e0f8eb02c4dda431677da688
Floweryu <im.zhangjunfeng@qq.com>
2022-07-31 13:10:42 +0800

    sentinel demo

---
+++ sentinel/src/main/java/com/floweryu/sentinel/Application.java
@@ -0,0 +1,15 @@
+package com.floweryu.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Floweryu
+ * @date 2022/6/26 21:33
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ sentinel/src/main/java/com/floweryu/sentinel/controller/SentinelController.java
@@ -0,0 +1,27 @@
+package com.floweryu.sentinel.controller;
+
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Floweryu
+ * @date 2022/6/26 21:38
+ */
+
+@RestController
+public class SentinelController {
+    
+    @SentinelResource(value = "hello", blockHandler = "helloBlock")
+    @GetMapping("/hello")
+    public String hello(String name) {
+        SphU.entry("sss");
+        return "hello" + name;
+    }
+    
+    public String helloBlock(String name, BlockException ex) {
+        return "error" + ex;
+    }
+}
