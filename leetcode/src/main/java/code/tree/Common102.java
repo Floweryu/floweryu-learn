378d6c20ddfddc2c7b4f2d015f9634760126cd89
zhangjunfeng <zhangjunfeng1@xiaomi.com>
2023-05-05 20:16:01 +0800

    二叉树层序遍历

--- leetcode/src/main/java/code/tree/Common102.java
+++ leetcode/src/main/java/code/tree/Common102.java
@@ -1,13 +1,39 @@
 package code.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
+ * 102. 二叉树的层序遍历
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/
  * @author: zhangjunfeng
  * @createTime: 2023/04/28
  */
 public class Common102 {
     public List<List<Integer>> levelOrder(TreeNode root) {
-
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            // 记录每次层个数
+            int size = queue.size();
+            // 每一层放入一个列表
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                tmp.add(poll.val);
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
     }
 }
