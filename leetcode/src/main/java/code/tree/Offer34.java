fee2bd92037f2593d63c6d7951193e1512c707e0
zhangjunfeng <zhangjunfeng1@xiaomi.com>
2023-05-08 20:33:35 +0800

    二叉树中和为某一值的路径

--- leetcode/src/main/java/code/tree/Offer34.java
+++ leetcode/src/main/java/code/tree/Offer34.java
@@ -1,7 +1,6 @@
 package code.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 剑指 Offer 34. 二叉树中和为某一值的路径
@@ -33,5 +32,54 @@ public class Offer34 {
         }
         // 如果当前叶子节点不满足target, 需要把他移除, 该节点不影响其他节点
         arr.remove(arr.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum2(TreeNode root, int target) {
+        // 存储最终结果
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        // 存储节点的父结点, 便于寻找完成路径 key: 节点 value: 父结点
+        Map<TreeNode, TreeNode> map = new HashMap<>();
+        // 层序遍历队列存储节点
+        Queue<TreeNode> queueNode = new LinkedList<>();
+        // 存储层序遍历到当前节点时，前面已经累加的sum
+        Queue<Integer> sumQueue = new LinkedList<>();
+        queueNode.offer(root);
+        sumQueue.offer(0);
+        while (!queueNode.isEmpty()) {
+            TreeNode poll = queueNode.poll();
+            int rec = sumQueue.poll() + poll.val;
+
+            if (poll.left == null && poll.right == null && rec == target) {
+                getPath(poll, res, map);
+            } else {
+                if (poll.left != null) {
+                    // 记录left的父结点
+                    map.put(poll.left, poll);
+                    queueNode.offer(poll.left);
+                    sumQueue.offer(rec);
+                }
+                if (poll.right != null) {
+                    map.put(poll.right, poll);
+                    queueNode.offer(poll.right);
+                    sumQueue.offer(rec);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 根据叶节点获取完整路径
+     */
+    public void getPath(TreeNode node, List<List<Integer>> res, Map<TreeNode, TreeNode> map) {
+        LinkedList<Integer> tmp = new LinkedList<>();
+        while (node != null) {
+            tmp.addFirst(node.val);
+            node = map.get(node);
+        }
+        res.add(tmp);
     }
 }
