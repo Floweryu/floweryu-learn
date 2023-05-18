bae73ee3b11c8a0ff25f2f51c8657864319ccebd
zhangjunfeng <zhangjunfeng1@xiaomi.com>
2023-05-18 11:55:29 +0800

    剑指 Offer 41. 数据流中的中位数

--- leetcode/src/main/java/code/stack/MedianFinder.java
+++ leetcode/src/main/java/code/stack/MedianFinder.java
@@ -9,30 +9,31 @@ import java.util.PriorityQueue;
  * @createTime: 2023/05/17
  */
 public class MedianFinder {
+    PriorityQueue<Integer> small;
+    PriorityQueue<Integer> big;
 
-    PriorityQueue<Integer> pq;
     public MedianFinder() {
-        pq = new PriorityQueue<>();
+        // 将排序后的数据流分为两部分
+        // 小顶堆存储排序后大的那部分值, 这样小顶堆能直接获取大的那部分最小值
+        // 大顶堆存储排序后小的那部分值, 这样大顶堆能直接获取小的那部分最大值
+        small = new PriorityQueue<>();
+        big = new PriorityQueue<>((o1, o2) -> (o2 - o1));
     }
 
     public void addNum(int num) {
-        pq.offer(num);
+        big.offer(num);
+        small.offer(big.poll());
+        if (big.size() + 1 < small.size()) {
+            // 如果大顶堆元素比小顶堆的少两个，则把小顶堆元素poll出来一个放入大顶堆
+            // 永远保证大顶堆和小顶堆元素最接近中位数
+            big.offer(small.poll());
+        }
     }
 
     public double findMedian() {
-        int size = pq.size();
-        if (size % 2 == 0) {
-            int index = size / 2;
-            while (index-- > 1) {
-                pq.poll();
-            }
-            return (pq.poll() + pq.poll()) / 2;
-        } else {
-            int index = size / 2 + 1;
-            while (index-- > 1) {
-                pq.poll();
-            }
-            return pq.poll();
-        }
+        // 说明是奇数
+        if (small.size() > big.size()) return small.peek();
+        // 偶数直接去平均值
+        return (small.peek() + big.peek()) / 2.0;
     }
 }
