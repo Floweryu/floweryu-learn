6863d7f93192582ab10f5137722631d43d7c1f2b
zhangjunfeng <zhangjunfeng1@xiaomi.com>
2023-05-22 13:01:52 +0800

    剑指 Offer 39. 数组中出现次数超过一半的数字

---
+++ leetcode/src/main/java/code/hash/Offer39.java
@@ -0,0 +1,30 @@
+package code.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 剑指 Offer 39. 数组中出现次数超过一半的数字
+ * https://leetcode.cn/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/
+ * @author: zhangjunfeng
+ * @createTime: 2023/05/22
+ */
+public class Offer39 {
+    public static int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int halfLength = nums.length / 2;
+        for (int num : nums) {
+            Integer times = map.getOrDefault(num, 0);
+            map.put(num, ++times);
+            if (times > halfLength) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,3,2,2,2,5,4,2};
+        majorityElement(arr);
+    }
+}
