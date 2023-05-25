b236b506d6f79a7eebde0c3aa53642fa2f53b774
zhangjunfeng <zhangjunfeng1@xiaomi.com>
2023-05-25 11:44:27 +0800

    200. 岛屿数量2

--- leetcode/src/main/java/code/dfs/Common200.java
+++ leetcode/src/main/java/code/dfs/Common200.java
@@ -8,7 +8,7 @@ package code.dfs;
  */
 public class Common200 {
     public int numIslands(char[][] grid) {
-        int count = 0
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 // 从陆地开始递归，
