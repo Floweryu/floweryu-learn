5c2ec58b82e45f71a94ce1dc97c3b37af0c9e8c1
zhangjunfeng <im.zhangjunfeng@qq.com>
2023-08-25 17:43:03 +0800

    LCR 086. 分割回文串

---
+++ leetcode/src/main/java/code/dfs/Offer086II.java
@@ -0,0 +1,23 @@
+package code.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LCR 086. 分割回文串
+ * https://leetcode.cn/problems/M99OJA/?envType=study-plan-v2&envId=coding-interviews-special
+ * 给定一个字符串 s ，请将 s 分割成一些子串，使每个子串都是 回文串 ，返回 s 所有可能的分割方案。
+ * 回文串 是正着读和反着读都一样的字符串。
+ * @author: zhangjunfeng
+ * @createTime: 2023/08/25
+ */
+public class Offer086II {
+
+    private List<List<String>> ans = new ArrayList<>();
+    public String[][] partition(String s) {
+        return new String[][]{};
+    }
+
+    public void backTrack(String s) {
+    }
+}
