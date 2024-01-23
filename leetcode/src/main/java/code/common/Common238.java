54260c0edbb766534424fabc44568984937a8170
floweryu <floweryu@qq.com>
2024-01-23 20:13:39 +0800

    238. 除自身以外数组的乘积

---
+++ leetcode/src/main/java/code/common/Common238.java
@@ -0,0 +1,36 @@
+package code.common;
+
+/**
+ * 238. 除自身以外数组的乘积
+ * https://leetcode.cn/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=top-interview-150
+ * @author: zhangjunfeng
+ * @date: 2024/01/22
+ */
+public class Common238 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] leftAns = new int[nums.length];
+        int[] rightAns = new int[nums.length];
+
+
+        int tmp = 1;
+        // 计算i左侧的乘积
+        for (int i = 0; i < nums.length; i++) {
+            leftAns[i] = tmp;
+            tmp *= nums[i];
+        }
+
+        tmp = 1;
+        // 计算i右侧的乘积
+        for (int i = nums.length - 1; i >= 0; i--) {
+            rightAns[i] = tmp;
+            tmp *= nums[i];
+        }
+
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = rightAns[i] * leftAns[i];
+        }
+
+        return res;
+    }
+}
