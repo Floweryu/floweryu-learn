da7a71b554e7348c6dc5bede828fd1e96934c000
floweryu <floweryu@qq.com>
2024-01-24 10:02:44 +0800

    238. 除自身以外数组的乘积2

--- leetcode/src/main/java/code/common/Common238.java
+++ leetcode/src/main/java/code/common/Common238.java
@@ -33,4 +33,22 @@ public class Common238 {
 
         return res;
     }
+
+    public int[] productExceptSelf2(int[] nums) {
+        int[] answer = new int[nums.length];
+        int tmp = 1;
+        // 计算i左侧的乘积
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = tmp;
+            tmp *= nums[i];
+        }
+
+        tmp = 1;
+        // 计算i右侧的乘积
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] *= tmp;
+            tmp *= nums[i];
+        }
+        return answer;
+    }
 }
