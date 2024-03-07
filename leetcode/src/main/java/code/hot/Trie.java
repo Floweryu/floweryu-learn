a06740676439880457ab1be8afded98fb6ff0233
Floweryu <floweryu@qq.com>
2024-03-07 16:13:23 +0800

    208. 实现 Trie (前缀树)

---
+++ leetcode/src/main/java/code/hot/Trie.java
@@ -0,0 +1,55 @@
+package code.hot;
+
+/**
+ * 208. 实现 Trie (前缀树)
+ * @author Floweryu
+ * @date 2024/3/7 11:38:20
+ */
+public class Trie {
+
+    class TrieNode {
+        boolean word = false;
+        TrieNode[] children = new TrieNode[26];
+    }
+
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                cur.children[index] = new TrieNode();
+            }
+            cur = cur.children[index];
+        }
+        cur.word = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                return false;
+            }
+            cur = cur.children[index];
+        }
+        return cur.word;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int index = prefix.charAt(i) - 'a';
+            if (cur.children[index] == null) {
+                return false;
+            }
+            cur = cur.children[index];
+        }
+        return true;
+    }
+}
