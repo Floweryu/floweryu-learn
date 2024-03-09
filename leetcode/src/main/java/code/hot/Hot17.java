5ced81a0c6852fa32486fa7afd188b07243c355f
Floweryu <floweryu@qq.com>
2024-03-09 17:21:16 +0800

    17. 电话号码的字母组合

--- leetcode/src/main/java/code/hot/Hot17.java
+++ leetcode/src/main/java/code/hot/Hot17.java
@@ -12,7 +12,9 @@ public class Hot17 {
     private List<String> ans = new ArrayList<>();
     HashMap<Character, String> map = new HashMap<>();
     public List<String> letterCombinations(String digits) {
-
+        if (digits.length() == 0) {
+            return ans;
+        }
         map.put('2', "abc");
         map.put('3', "def");
         map.put('4', "ghi");
@@ -21,13 +23,20 @@ public class Hot17 {
         map.put('7', "pqrs");
         map.put('8', "tuv");
         map.put('9', "wxyz");
-        dfs(digits, 0, new ArrayList<>());
+        dfs(digits, 0, new StringBuilder());
+        return ans;
     }
 
-    private void dfs(String digits, int index, List<String> arr) {
-        for (int i = index; i < digits.length(); i++) {
-            String dict = map.get(digits.charAt(i));
-
+    private void dfs(String digits, int index, StringBuilder sb) {
+        if (sb.length() == digits.length()) {
+            ans.add(new String(sb));
+            return;
+        }
+        String dict = map.get(digits.charAt(index));
+        for (int i = 0; i < dict.length(); i++) {
+            sb.append(dict.charAt(i));
+            dfs(digits, index + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
